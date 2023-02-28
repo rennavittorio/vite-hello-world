1a7e87e774d0e762f4aa6c17e356c2d5a5a5bcdf
rennavittorio <renna.vittorio@gmail.com>
2023-02-28 15:06:58 +0100

    START new project created via VITE

---
+++ vite-first-project/src/App.vue
@@ -0,0 +1,27 @@
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+  component: {
+    AppTitle
+  },
+
+  data() {
+    return {
+
+    }
+  }
+}
+
+</script>
+
+<template>
+
+  <AppTitle></AppTitle>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ vite-first-project/src/components/AppTitle.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    data(){
+        return {
+            message: 'La mia prima app con Vite!'
+        }
+    }
+}
+
+
+</script>
+
+<template>
+
+    <div>
+        <h1 class="title">
+            {{ message }}
+        </h1>
+    </div>
+
+</template>
+
+
+<style scoped>
+.title {
+    color: red;
+    border: 2px solid red;
+}
+
+
+</style>
